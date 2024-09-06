b07f34f16ef6ac9110665435902c1af13788877f
mryeminaung <yeminaung013@gmail.com>
2024-09-07 02:16:25 +0630

    useRef for fields to avoid re-render

--- src/components/academind/NewGoal.tsx
+++ src/components/academind/NewGoal.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { GoalProps } from "../../App";
 import { v4 as uuidv4 } from "uuid";
 
@@ -7,24 +7,53 @@ type NewGoalProps = {
 };
 
 const NewGoal = ({ addGoal }: NewGoalProps) => {
-    const [goal, setGoal] = useState<{ title: string; description: string }>({
-        title: "",
-        description: "",
-    });
+    // const [goal, setGoal] = useState<{ title: string; description: string }>({
+    //     title: "",
+    //     description: "",
+    // });
+
+    const titleRef = useRef<HTMLInputElement>(null);
+    const descriptionRef = useRef<HTMLInputElement>(null);
 
     const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
         e?.preventDefault(); // ? means that e obj empty or not
-        addGoal({ id: uuidv4(), ...goal });
-        setGoal({ title: "", description: "" }); // clear form fields
+
+        const newGoal: GoalProps = {
+            id: uuidv4(),
+            title: titleRef.current?.value,
+            description: descriptionRef.current?.value,
+        };
+        addGoal(newGoal);
+
+        e.currentTarget.reset(); // clear form fields
+
+        // addGoal({ id: uuidv4(), ...goal });
+        // setGoal({ title: "", description: "" }); // clear form fields
     };
 
-    const handleGoal: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-        const { name, value } = e.target;
-        setGoal({ ...goal, [name]: value });
-    };
+    // const handleGoal: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    //     const { name, value } = e.target;
+    //     setGoal({ ...goal, [name]: value });
+    // };
 
     return (
         <form onSubmit={handleSubmit}>
+            <p>
+                <label htmlFor="title">Title Ref</label>
+                <input ref={titleRef} name="title" type="text" id="title" />
+            </p>
+            <p>
+                <label htmlFor="description">Description Ref</label>
+                <input
+                    ref={descriptionRef}
+                    name="description"
+                    type="text"
+                    id="description"
+                />
+            </p>
+
+            {/* 
+            
             <p>
                 <label htmlFor="title">Your goal</label>
                 <input
@@ -46,6 +75,8 @@ const NewGoal = ({ addGoal }: NewGoalProps) => {
                     id="description"
                 />
             </p>
+             */}
+
             <button>Add Goal</button>
         </form>
     );
